ef2a1332d1d8a1cc15871068ba0452f9e0f90d55
Gary Hill <ghill315@att.net>
2015-02-07 11:21:53 -0600

    Attempt to refactor to MVVM

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
-function initialize() {
+var model = function(data) {
   var mapOptions = {
-    center: { lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: { lat: ko.observable(data.lat),
+       lng: ko.observable(data.lng)},
+    zoom: ko.observable(data.zoom)
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+};
+
+var viewModel = function() {
+  this.currentMarker = ko.observable(new model({
+      lat: 38.561821,
+      lng: -89.898225,
+      zoom: 12
+    })
+  )
+};
+
+var view = {
+  init : function() {
+    var map = new google.maps.Map(document.getElementById('map-canvas'), model.mapOptions);
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+  };
+};
+
+ko.applyBindings(view.init());
