6428ecd2457f8b7ca2ca4af29adefa451c98fa83
Gary Hill <ghill315@att.net>
2015-02-08 14:42:12 -0600

    Create list of map points

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ var model = {
 };
 
 var viewModel = function() {
-  var map, bounds;
+  var map, bounds, markerArray;
 
   //Initialize map location, set as IIFE to kick off immediately
   var initMap = function() {
@@ -62,6 +62,8 @@ var viewModel = function() {
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     bounds = new google.maps.LatLngBounds();
+
+    markerArray = ko.observableArray();
 
     //add markers
     var markerList = model.markers;
@@ -90,8 +92,12 @@ var viewModel = function() {
       bounds.extend(markPos);
       map.fitBounds(bounds);
       map.setCenter(bounds.getCenter());
+
+      markerArray.push(marker);
     }
+    console.log(markerArray());
   }();
+
 
 };
 
