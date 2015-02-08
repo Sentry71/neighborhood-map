a461493a418be2ce23cc29f85ebb7f1516e04e96
Gary Hill <ghill315@att.net>
2015-02-07 22:20:52 -0600

    Add marker data and code to display markers

--- js/app.js
+++ js/app.js
@@ -1,19 +1,98 @@
-var model = function(data) {
-  //
+var model = {
+  currentMarker: null,
+  markers: [
+    {
+      title: 'The Butterfly House',
+      lat: 38.664640,
+      lng: -90.542808
+    },
+    {
+      title: 'Missouri Botanical Garden',
+      lat: 38.612784,
+      lng: -90.259364
+    },
+    {
+      title: 'City Museum',
+      lat: 38.633330,
+      lng: -90.200525
+    },
+    {
+      title: 'Endangered Wolf Center',
+      lat: 38.526428,
+      lng: -90.560422
+    },
+    {
+      title: 'The Magic House',
+      lat: 38.573845,
+      lng: -90.405244
+    },
+    // {
+    //   title: 'Meramec Caverns',
+    //   lat: 38.245292,
+    //   lng: -91.090536
+    // },
+    {
+      title: 'Missouri History Museum',
+      lat: 38.645254,
+      lng: -90.285770
+    },
+    {
+      title: 'St Louis Science Center',
+      lat: 38.628936,
+      lng: -90.270626
+    },
+    {
+      title: 'St Louis Zoo',
+      lat: 38.635024,
+      lng: -90.290711
+    },
+    {
+      title: 'World Bird Sanctuary',
+      lat: 38.536997,
+      lng: -90.533453
+    }
+  ]
 };
 
 var viewModel = function() {
-  var map;
+  var map, bounds;
 
   //Initialize map location, set as IIFE to kick off immediately
   var initMap = function() {
+    //create map
     var mapOptions = {
-      center: { lat: 38.561821,
-         lng: -89.898225},
-      zoom: 12
+      center: { lat: 38.633391,
+         lng: -90.200547},
+      zoom: 11,
+      disableDefaultUI: true
     };
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  }();
+
+    bounds = new google.maps.LatLngBounds();
+
+    //add markers
+    var markerList = model.markers;
+    for(var x = 0; x < markerList.length; x++) {
+      //TODO: research delayed marker drop
+      //setTimeout(function() {
+        var markPos = new google.maps.LatLng(
+          markerList[x].lat,
+          markerList[x].lng
+        );
+        var marker = new google.maps.Marker({
+          position: markPos,
+          map: map,
+          title: markerList[x].title,
+          animation: google.maps.Animation.DROP
+        });
+        bounds.extend(markPos);
+        map.fitBounds(bounds);
+        map.setCenter(bounds.getCenter());
+      //}, x * 200);
+    }
+  };
+
+initMap();
 };
 
 ko.applyBindings(new viewModel());
