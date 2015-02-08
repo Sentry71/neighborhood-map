726e7d6920908a0ce4950509d3427ac3d4a54eff
Gary Hill <ghill315@att.net>
2015-02-07 19:13:02 -0600

    Complete refactor of JS to MVVM format

--- js/app.js
+++ js/app.js
@@ -1,26 +1,19 @@
 var model = function(data) {
-  var mapOptions = {
-    center: { lat: ko.observable(data.lat),
-       lng: ko.observable(data.lng)},
-    zoom: ko.observable(data.zoom)
-  };
+  //
 };
 
 var viewModel = function() {
-  this.currentMarker = ko.observable(new model({
-      lat: 38.561821,
-      lng: -89.898225,
+  var map;
+
+  //Initialize map location, set as IIFE to kick off immediately
+  var initMap = function() {
+    var mapOptions = {
+      center: { lat: 38.561821,
+         lng: -89.898225},
       zoom: 12
-    })
-  )
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  }();
 };
 
-var view = {
-  init : function() {
-    var map = new google.maps.Map(document.getElementById('map-canvas'), model.mapOptions);
-
-    google.maps.event.addDomListener(window, 'load', initialize);
-  };
-};
-
-ko.applyBindings(view.init());
+ko.applyBindings(new viewModel());
